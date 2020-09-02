f27c62210aeb8e35340b7ed8148fb840640fc474
THXC <cwpenguinmaster2005@gmail.com>
2020-09-02 03:38:16 -0400

    Create decompress.py

---
+++ decompress.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+
+# coding=utf-8
+
+import re
+
+from compress import symbols
+
+
+
+def decompress(compressed_content):
+
+
+
+    decompressed_content = '' 
+
+
+
+    return compressed_content
+
+    
