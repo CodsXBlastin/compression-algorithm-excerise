9361a58960f55de2ea9c015b24a6c57df230a3a8
THXC <cwpenguinmaster2005@gmail.com>
2020-09-02 03:37:37 -0400

    alternative solution

---
+++ compress.py
@@ -0,0 +1,31 @@
+# !/usr/bin/python
+
+# coding=utf-8
+
+import re
+
+
+
+symbols = {
+
+#   key             :   symbols[key]
+
+    "implementation":   "🤯",
+    "practicality"  :   '🤩',
+    "better"        :   '😅',
+    "than"          :   '😘',
+    "Although"      :   "🥺",
+
+}
+
+
+
+def compress(content):
+
+
+
+    compressed_content = ''    
+
+
+
+    return content
